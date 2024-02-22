5b22b89d20334bf49abc7a2ef88a791206344edf
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-02-22 15:39:29 +0530

    added border style to button in hero section.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight"
-// import { Button } from "./ui/moving-border";
+import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
@@ -20,12 +20,12 @@ function HeroSection() {
             >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
             <div className="mt-4">
                 <Link href={"/courses"}>
-                    {/* <Button
+                    <Button
                     borderRadius="1.75rem"
                     className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
                     >
                     Explore courses
-                    </Button> */}
+                    </Button>
                 </Link>
             </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "tailwindcss";
-
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+ 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -31,6 +34,20 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
